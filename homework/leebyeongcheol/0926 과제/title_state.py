e5d7185c07ca8a8e6cc1b2ba326d2726cce287cd
leebyeongcheol <32187933+leebyeongcheol@users.noreply.github.com>
2017-10-11 15:25:00 +0900

    10//11 homework

--- homework/leebyeongcheol/0926 과제/title_state.py
+++ homework/leebyeongcheol/0926 과제/title_state.py
@@ -1,5 +1,8 @@
 from pico2d import*
 import game_framework
+import main_state
+
+
 
 name = "TitleState"
 image = None
@@ -21,6 +24,8 @@ def handle_events():
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
 
 
 def draw():
