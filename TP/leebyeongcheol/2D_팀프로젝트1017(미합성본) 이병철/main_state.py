825cdd9d4a3583d51a565766f84625b640c46945
leebyeongcheol <32187933+leebyeongcheol@users.noreply.github.com>
2017-10-18 03:04:10 +0900

    TP_2~3주차(미합성본)
    점프를 구현하려고 시도했으나, 실패하여   Y축 최대, 최소 값을 정해놓고 수동점프로 구현(수정 예정) , 합성본으로 다시 올려드리겠습니다.

---
+++ TP/leebyeongcheol/2D_팀프로젝트1017(미합성본) 이병철/main_state.py
@@ -0,0 +1,163 @@
+from pico2d import *
+import game_framework
+import title_state
+import random
+
+
+
+class Mario:
+    image = None
+
+    LEFT_RUN, RIGHT_RUN, LEFT_STAND, RIGHT_STAND, UP_JUMP, DOWN, RIGHT_JUMP, LEFT_JUMP = 0, 1, 2, 3, 5, 4, 7, 6
+
+    def __init__(self):
+        self.x, self.y = 100, 84
+        self.frame = random.randint(0, 7)
+        self.dir = 1
+        self.state = self.RIGHT_STAND
+        self.run_frames = 0
+        self.stand_frames = 0
+        if Mario.image == None:
+            self.image = load_image('animation_sheet1.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+        if self.state == self.RIGHT_RUN:
+            self.x = min(800, self.x + 5)
+            self.x += 5
+        elif self.state == self.LEFT_RUN:
+            self.x = max(0, self.x - 5)
+            self.x -= 5
+        elif self.state == self.UP_JUMP:
+            self.y = min(600, self.y + 2)
+            self.y += 2
+        elif self.state == self.DOWN:
+            self.y = max(70, self.y - 2)
+            self.y -= 2
+      # elif self.state == self.RIGHT_JUMP:
+      #     self.x += 2#
+      #     self.y += 2
+        delay(0.03)
+
+   #def handle_left_run(self):
+   #    self.x -= 5
+   #    self.run_frames += 1
+   #    if self.x < 0:
+   #        self.state = self.RIGHT_RUN
+   #        self.x = 0
+   #    if self.run_frames == 100:
+   #        self.state = self.LEFT_STAND
+   #        self.stand_frames = 0
+
+   #def handle_left_stand(self):
+   #    self.stand_frames += 1
+   #    if self.stand_frames == 50:
+   #        self.state = self.LEFT_RUN
+   #        self.run_frames = 0
+
+   #def handle_right_run(self):
+   #    self.x += 5
+   #    self.run_frames += 1
+   #    if self.x > 800:
+   #        self.state = self.LEFT_RUN
+   #        self.x = 800
+   #    if self.run_frames == 100:
+   #        self.state = self.RIGHT_STAND
+   #        self.stand_frames = 0
+
+   #def handle_right_stand(self):
+   #    self.stand_frames += 1
+   #    if self.stand_frames == 50:
+   #        self.state = self.RIGHT_RUN
+   #        self.run_frames = 0
+#
+   #handle_state = {
+   #    LEFT_RUN: handle_left_run,
+   #    RIGHT_RUN: handle_right_run,
+   #    LEFT_STAND: handle_left_stand,
+   #    RIGHT_STAND: handle_right_stand
+   #}
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, self.state * 100, 100, 100, self.x, self.y)
+
+class BackGround:
+    def __init__(self):
+        self.image = load_image("background.png")
+    def draw(self):
+        self.image.draw(400,300)
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 26)
+
+class Object:
+    def __init__(self):
+        self.image = load_image('object_LongPipe.png')
+    def draw(self):
+        self.image.draw(600,114)
+
+
+def enter():
+    global mario, grass, object, background
+    mario = Mario()
+    grass = Grass()
+    object = Object()
+    background = BackGround()
+
+
+def exit():
+    global mario, grass, object, background
+    del(mario)
+    del(grass)
+    del(object)
+    del(background)
+
+
+def handle_events():
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_LEFT:
+                mario.state = mario.LEFT_RUN
+            if event.key == SDLK_RIGHT:
+                mario.state = mario.RIGHT_RUN
+            if event.key == SDLK_UP:
+                mario.state = mario.UP_JUMP
+            if event.key == SDLK_DOWN:
+                mario.state = mario.DOWN
+          # if event.key == SDLK_UP and event.key == SDLK_RIGHT:
+          #     mario.state = mario.RIGHT_JUMP
+          # if event.key == SDLK_UP and event.key == SDLK_LEFT:
+          #     mario.state = mario.LEFT_JUMP
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                mario.state = mario.LEFT_STAND
+            if event.key == SDLK_RIGHT:
+                mario.state = mario.RIGHT_STAND
+            if event.key == SDLK_UP:
+                mario.state = mario.RIGHT_JUMP
+            if event.key == SDLK_DOWN:
+                mario.state = mario.LEFT_JUMP
+
+def update():
+    mario.update()
+
+def draw():
+    clear_canvas()
+    background.draw()
+    grass.draw()
+    object.draw()
+    mario.draw()
+    update_canvas()
+
+
+
